91ec39d27d479cc23c0d18f43eb1e884dd3887cb
Fadeta <fadeta287@gmail.com>
2024-05-01 18:53:06 +0700

    commit

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:koimedic/screens/authpage.dart';
+import 'package:koimedic/screens/fade_animation.dart';
 import 'package:koimedic/screens/homepage.dart';
+import 'package:koimedic/widget/common.dart';
 
 class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
@@ -24,21 +28,53 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            FadeAnimation(
+              delay: 1,
+              child: IconButton(
+                onPressed: () {
+                  Get.to(const Authpage());
+                },
+                icon: const Icon(
+                  CupertinoIcons.back,
+                  size: 35,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FadeAnimation(
+                    delay: 1.3,
+                    child: Text(
+                      "Selamat datang kembali!",
+                      style: Common().titelTheme,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             TextField(
               controller: email,
               decoration: const InputDecoration(hintText: 'Enter Email'),
+            ),
+            const SizedBox(
+              height: 10,
             ),
             TextField(
               controller: password,
               decoration: const InputDecoration(hintText: 'Enter Password'),
               obscureText: true,
+            ),
+            const SizedBox(
+              height: 10,
             ),
             ElevatedButton(
               onPressed: () async {
