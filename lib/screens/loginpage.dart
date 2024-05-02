f23f4527700df4dfa1e73abf43230aa302172b8a
Fadeta <fadeta287@gmail.com>
2024-05-02 14:22:27 +0700

    commit

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -28,61 +28,127 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FadeAnimation(
-              delay: 1,
-              child: IconButton(
-                onPressed: () {
-                  Get.to(const Authpage());
-                },
-                icon: const Icon(
-                  CupertinoIcons.back,
-                  size: 35,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FadeAnimation(
-                    delay: 1.3,
-                    child: Text(
-                      "Selamat datang kembali!",
-                      style: Common().titelTheme,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FadeAnimation(
+                  delay: 1,
+                  child: IconButton(
+                    onPressed: () {
+                      Get.to(const Authpage());
+                    },
+                    icon: const Icon(
+                      CupertinoIcons.back,
+                      size: 35,
                     ),
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FadeAnimation(
+                        delay: 1.3,
+                        child: Text(
+                          "Selamat datang!",
+                          style: Common().titelTheme,
+                        ),
+                      ),
+                      FadeAnimation(
+                        delay: 1.6,
+                        child: Text(
+                          "Masuk untuk periksa Koi anda!",
+                          style: Common().titelTheme,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: email,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email',
+                          contentPadding: const EdgeInsets.all(18),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        controller: password,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password',
+                          hintStyle: Common().hinttext,
+                          contentPadding: const EdgeInsets.all(18),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                flag = !flag;
+                              });
+                            },
+                            icon: const Icon(Icons.remove_red_eye_outlined),
+                          ),
+                        ),
+                        obscureText: flag,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            signIn();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(color: Colors.white30),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 50),
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: "Urbanist-SemiBold",
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            TextField(
-              controller: email,
-              decoration: const InputDecoration(hintText: 'Enter Email'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: password,
-              decoration: const InputDecoration(hintText: 'Enter Password'),
-              obscureText: true,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                signIn();
-              },
-              child: const Text("Login"),
-            ),
-          ],
+          ),
         ),
       ),
     );
