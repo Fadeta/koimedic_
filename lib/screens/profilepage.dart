179ac57695a138b7220a28e29dad68fa4aa8ef07
Fadeta <fadeta287@gmail.com>
2024-05-11 22:58:19 +0700

    commit profilepage

--- lib/screens/profilepage.dart
+++ lib/screens/profilepage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:koimedic/screens/homepage.dart';
 import 'package:koimedic/screens/loginpage.dart';
 
 class Profilepage extends StatefulWidget {
@@ -13,23 +14,92 @@ class Profilepage extends StatefulWidget {
 class _ProfilepageState extends State<Profilepage> {
   final user = FirebaseAuth.instance.currentUser;
 
-  Future<void> signout() async {
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
     Get.to(const Loginpage());
+  }
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Get.off(() => const Homepage());
+        break;
+      case 1:
+        Get.to(() => const Profilepage());
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Profilepage"),
+        title: const Text("Profile Page"),
       ),
       body: Center(
-        child: Text(user != null ? user!.email ?? 'No Email' : 'Not Logged In'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundColor: Colors.blueGrey,
+              radius: 60,
+              child: Icon(
+                Icons.person,
+                size: 80,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Astrokoi Farm", // Ganti dengan nama farm Anda
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              user != null ? user!.email ?? 'No Email' : 'Not Logged In',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => signout(),
+        onPressed: () => signOut(),
         child: const Icon(Icons.logout_rounded),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey[500],
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
